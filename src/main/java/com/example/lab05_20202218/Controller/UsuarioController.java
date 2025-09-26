5a9ec09b36a35d035ef09b60dcb650bf50ea3724
daniel_2 <dani6468@gmail.com>
2025-09-25 21:06:39 -0500

    registro

--- src/main/java/com/example/lab05_20202218/Controller/UsuarioController.java
+++ src/main/java/com/example/lab05_20202218/Controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.example.lab05_20202218.Controller;
 
+import com.example.lab05_20202218.Entity.Usuario;
 import com.example.lab05_20202218.Repository.UsuarioRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsuarioController {
@@ -19,4 +24,25 @@ public class UsuarioController {
         model.addAttribute("usuarios", usuarioRepository.findAll());
         return "index";
     }
+
+    @GetMapping("/registro")
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String procesarFormulario(
+            @Valid @ModelAttribute("usuario") Usuario usuario,
+            BindingResult result,
+            Model model) {
+
+        if (result.hasErrors()) {
+            return "registro";
+        }
+
+        return "redirect:/inicio";
+
+    }
+
 }
